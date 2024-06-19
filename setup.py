edeb022b1bbebd2c41c01014262c15b8880e3b9e
Sagar Khadakban <reach4sagar@yahoo.com>
2024-06-19 20:36:05 +0530

    updated with jupyter package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-"""HYPEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -13,13 +13,13 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+    return requirements
 
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
     author='sagar khadakban',
     author_email='reach4sagar@gmail.com',
-    install_requires= get_requirements('requirements.txt')  #["scikit-learn","pandas","numpy"],
+    install_requires= get_requirements('requirements.txt'),  #["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
